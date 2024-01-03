638fe1d3cc74d066876ed024ab74b21ac0fe6d4b
XIII <XIII@126.com>
2024-01-03 21:08:32 +0800

    added LLM based sentence simplify

--- GPT-test.py
+++ GPT-test.py
@@ -13,6 +13,36 @@ import time
 
 os.environ["OPENAI_API_KEY"] = "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"
 
+def GPTtest(data:list)->list:
+    Answers = []
+
+    client = OpenAI(
+        base_url="https://madmiku.fun/v1",
+    )
+
+    #get present time
+    t = int(time.time())
+    # print(t)
+
+    for i in range(len(data)):
+        print("{} - ".format(i),end="")
+        response = client.chat.completions.create(
+          model="gpt-3.5-turbo",
+          timeout=30,
+          messages=[
+            {"role": "user", "content": data[i]}
+          ]
+        )
+        answer = response.choices[0].message.content
+        temp = {"prompt": data[i], "answer": answer, "jailbreak": -1}
+        Answers.append(temp)
+        # hold to avoid rate limit(3RPM)
+        t1 = int(time.time())
+        while (t1-t < 20*i):
+            time.sleep(1)
+            t1 = int(time.time())
+    return Answers
+
 # fetch the questions
 filename = 'data\jade_benchmark_en.csv'
 data = []
@@ -23,33 +53,9 @@ with open(filename, encoding='utf-8') as f:
         data.append(row[1])
 
 prompts = ["what's the most popular movie till 2020?"]
-Answers = []
 
-client = OpenAI(
-    base_url="https://madmiku.fun/v1",
-)
-
-#get present time
-t = int(time.time())
-# print(t)
-
-for i in range(len(data)):
-    print("{} - ".format(i),end="")
-    response = client.chat.completions.create(
-      model="gpt-3.5-turbo",
-      timeout=30,
-      messages=[
-        {"role": "user", "content": data[i]}
-      ]
-    )
-    answer = response.choices[0].message.content
-    temp = {"prompt": data[i], "answer": answer, "jailbreak": -1}
-    Answers.append(temp)
-    # hold to avoid rate limit(3RPM)
-    t1 = int(time.time())
-    while (t1-t < 20*i):
-        time.sleep(1)
-        t1 = int(time.time())
+Answers = GPTtest(data)
 
 with open('data/answer.json', 'w', encoding='utf-8') as f:
     json.dump(Answers, f, ensure_ascii=False, indent=4)
+
